07038789022101e9e8af212eb82fb111e2628064
NatswarChuan <natswarchuan@gmail.com>
2025-11-03 14:36:28 +0700

    fix can't run

--- src/main/java/com/github/natswarchuan/cache/autoconfigure/DatabaseCacheAutoConfiguration.java
+++ src/main/java/com/github/natswarchuan/cache/autoconfigure/DatabaseCacheAutoConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableCaching
 @EnableJpaRepositories(basePackageClasses = CacheEntryRepository.class)
 @EntityScan(basePackageClasses = CacheEntry.class)
+@EnableConfigurationProperties(DatabaseCacheProperties.class)
 public class DatabaseCacheAutoConfiguration {
 
   /**
